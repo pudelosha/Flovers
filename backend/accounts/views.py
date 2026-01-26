1edaf4603377fa61ae027b6e874f7b7270c3eb1f
pudelosha <pudel1985@gmail.com>
2026-01-26 18:56:56 +0100

    Updated auth to pass account create date to Profile screen

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -95,6 +95,7 @@ class LoginView(APIView):
                 "email": user.email,
                 "first_name": user.first_name,
                 "last_name": user.last_name,
+                "date_joined": user.date_joined,
             },
         }
         return ok("Login successful.", data)
--- mobile/src/api/services/auth.service.ts
+++ mobile/src/api/services/auth.service.ts
@@ -6,6 +6,7 @@ export type AuthUserInfo = {
   email?: string;
   first_name?: string;
   last_name?: string;
+  date_joined?: string;
 } | null;
 
 export type RegisterPayload = {
