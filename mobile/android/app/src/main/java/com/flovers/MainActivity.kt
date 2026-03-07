83c503cac5e1484bdc95a5501244a555510fc089
pudelosha <pudel1985@gmail.com>
2026-03-07 22:04:00 +0100

    Fix to MainActivity which caused an error

--- mobile/android/app/src/main/java/com/flovers/MainActivity.kt
+++ mobile/android/app/src/main/java/com/flovers/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.flovers
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(null)
+  }
 
   override fun getMainComponentName(): String = "flovers"
 
